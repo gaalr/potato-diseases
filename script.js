de6e8d7bd3ce2398515f27b9f53faddeb684dc68
gaalr <109805611+gaalr@users.noreply.github.com>
2025-01-29 09:01:11 +0100

    Key changes to code

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
 'use strict';
-
-const acc = document.getElementsByClassName('accordion');
-
-for (let i = 0; i < acc.length; i++) {
-	acc[i].addEventListener('click', function () {
-		this.classList.toggle('active');
-	});
-}
